5fbe81b4cff80b3b9ba053608e02ced484f271c2
Taketo Iseki <take2webservice@gmail.com>
2019-05-15 13:08:10 +0900

    add dotenv

--- app/Services/RedisService.js
+++ app/Services/RedisService.js
@@ -1,13 +1,19 @@
 var IORedis = require('ioredis')
+// load connection
+require('dotenv').config({ path: abs_path('/.env') })
+
 module.exports = class RedisService {
   static start() {
-    return new IORedis({
-      port: 16379, // Redis port
-      host: '127.0.0.1', // Redis host
-      family: 4, // 4 (IPv4) or 6 (IPv6)
-      //password: 'auth',
+    const args = {
+      port: process.env.REDIS_PORT ? process.env.REDIS_PORT : 6379, // Redis port
+      host: process.env.REDIS_HOST ? process.env.REDIS_HOST :'127.0.0.1', // Redis host
+      family: process.env.REDIS_IP_FAMILY ? process.env.REDIS_IP_FAMILY : 4, // 4 (IPv4) or 6 (IPv6)
       db: 0,
-    })
+    }
+    if (process.env.REDIS_PASSWORD) {
+      args.password = process.env.REDIS_PASSWORD
+    }
+    return new IORedis(args)
   }
 
   static close(redis) {
